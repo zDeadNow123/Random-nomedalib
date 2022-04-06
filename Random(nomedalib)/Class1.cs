34e3a52fdba978f039f4b2a60ab98f4d2655e948
zDeadNow123 <dinnuhgamer@gmail.com>
2022-04-06 10:45:15 -0300

    Adicionar arquivos de projeto.

---
+++ Random(nomedalib)/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_nomedalib_
+{
+    public class Class1
+    {
+    }
+}
